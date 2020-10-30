8d1c1e130a747325b941a018c244b4d90a848d28
Péter Sturm <sturm.peti@gmail.com>
2020-10-30 13:21:33 +0100

    Refactored EmailTypes endpoint

--- src/Lunitor.Notification.Web/Endpoints/EmailTypes/GetAll.EmailTypesResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using Lunitor.Notification.Web.Model;
-
-namespace Lunitor.Notification.Web.Endpoints.EmailTypes
-{
-    public class EmailTypesResponse
-    {
-        public EmailType[] EmailTypes { get; set; }
-    }
-}
